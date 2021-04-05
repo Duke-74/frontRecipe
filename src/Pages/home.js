8071998f36d10037b07ea99eebf449da063834d5
Max <white-max@mail.ru>
2021-04-05 18:12:04 +0300

    front v1

---
+++ src/Pages/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import CarouselBox from '../Components/CarouselBox'
+//import Carousel from 'react-bootstrap/Carousel'
+
+export default class home extends Component {
+    render() {
+        return (
+            <CarouselBox />
+        )
+    }
+}
---
+++ src/Pages/search.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SearchRecipe extends Component {
+    render() {
+        return (
+            <div>
+                SearchRecipe
+            </div>
+        )
+    }
+}
